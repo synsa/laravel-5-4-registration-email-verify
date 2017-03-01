24224233a3231a0132861217f71350e62a6929ff
Eddie Lopez <eddie.lopezjr@gmail.com>
2017-03-01 17:40:22 +0100

    Fixed the email verification

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -36,4 +37,14 @@ class LoginController extends Controller
     {
         $this->middleware('guest', ['except' => 'logout']);
     }
+
+
+    public function credentials(Request $request)
+{
+    return [
+        'email' => $request->email,
+        'password' => $request->password,
+        'verified' => 1,
+    ];
+}
 }
